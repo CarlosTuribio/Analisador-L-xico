fae8b4b51a4a27a0bbd98dfb77bae4db8da1ac5a
CarlosTuribio <60799275+CarlosTuribio@users.noreply.github.com>
2021-08-21 12:42:12 -0300

    Add files via upload

--- analisador léxico.py
+++ analisador léxico.py
@@ -1,4 +1,9 @@
 # Carlos Turibio
+def inArray(idenfity, simbolos):
+    for i in simbolos:
+        if idenfity == i[1]:
+            return True
+    return False
 
 def isnumber(value):
     try:
@@ -7,30 +12,63 @@ def isnumber(value):
          return False
     return True
 
+def findToken(identify, simbolos):
+    for i in simbolos:
+        if i[1] == identify:
+            return i[0]
+
+def symbol(identify, simbolos):
+    if len(simbolos) == 0:
+        simbolos.append([len(simbolos)+1, identify ])
+        return simbolos[-1][0]
+    else:
+        if inArray(identify, simbolos):
+            return findToken(identify, simbolos)
+
+        else:
+            simbolos.append([len(simbolos)+1, identify ])
+            return simbolos[-1][0]
+            
 
 def compile(entrada):
-    reservedW = ["while","do"]
-    operadores = ["<","=","+"]
-    indentifiers =  ["i","j"]
-
-    if (entrada[-1] == ";"):
-        ter = entrada.find(";")    
-    entrada = entrada[:-1].split(" ")
-    print (entrada)
-    print("Token, Identificação, Tamanho, Posição, ")
-    cLen = 0
-    for i,token in enumerate(entrada):
-        if token in reservedW:
-            print (token, ", Palavra Reservada, ",len(token), ", (0,",cLen, ");")
-        elif isnumber(token):
-            print (token, ", Constante, ",len(token), ", (0,",cLen, "); ")
-        elif token in operadores:
-            print (token, ", Operador, ",len(token), ", (0,",cLen, "); ")
-            
-        elif token in indentifiers:
-            print (token, ", Identificador, ",len(token), ", (0,",cLen, "); ")
-        cLen += len(token) + 1
     
-    print (token, ", Terminador, ",len(token), ", (0,",ter, ");")
+    cLen = 0
+    tokens = []
+    simbolos = []
+    indentifiers = ["i","j"]
+    operadores = ["<","=","+"]
+    reservedW = ["while","do"]
+    entrada = entrada[:-1].split(" ")
+    print("--------------Tokens--------------------")
+    print("Token, Identificação, Tamanho, Posição, ")
+    for i,token in enumerate(entrada):
+        tokensi = []
+        if token in reservedW:
+            tokensi.append(token)
+            tokensi.append("Palavra Reservada")
+        elif isnumber(token):
+            tokensi.append(token)
+            tokensi.append(["Identificador",symbol(token,simbolos)])
+        elif token in operadores:
+            tokensi.append(token)
+            tokensi.append("Operador")
+        elif token in indentifiers:
+            tokensi.append(token)
+            tokensi.append(["Identificador",symbol(token,simbolos)])
+
+
+        tokensi.append(len(token))
+        tokensi.append("(0,"+str(cLen)+")")
+
+        tokens.append(tokensi)
+        cLen += len(token) + 1
+    tokens.append([";", "Terminador", "1", "(0,"+str(cLen-1)+")"])
+    for i in tokens:    
+        print(i)
+    print("----------------------------------------\n")
+    print("-------------Símbolos-------------------")
+    for i in simbolos:
+        print(i)
+
 
 compile("while i < 100 do i = i + j;")
